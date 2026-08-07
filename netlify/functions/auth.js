244facca9854c3787a111545d45e13ff452609b9
igorch1955 <igorch1955@gmail.com>
2026-08-07 21:11:34 +0200

    Add files via upload
    Signed-off-by: igorch1955 <igorch1955@gmail.com>

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,1 +1,16 @@
+exports.handler = async function (event) {
+  const clientId = process.env.OAUTH_CLIENT_ID;
+  const host = event.headers['x-forwarded-host'] || event.headers.host;
+  const redirectUri = `https://${host}/.netlify/functions/callback`;
+  const authorizeUrl =
+    `https://github.com/login/oauth/authorize` +
+    `?client_id=${clientId}` +
+    `&scope=repo,user` +
+    `&redirect_uri=${encodeURIComponent(redirectUri)}`;
 
+  return {
+    statusCode: 302,
+    headers: { Location: authorizeUrl },
+    body: '',
+  };
+};
